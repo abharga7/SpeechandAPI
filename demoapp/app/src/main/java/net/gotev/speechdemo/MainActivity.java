d46efa9c2305a5348cdf8cfd8c778cca865e64bb
abharga7 <abharga7@asu.edu>
2017-09-24 04:13:51 -0700

    All initial changes
    Initial commit

---
+++ demoapp/app/src/main/java/net/gotev/speechdemo/MainActivity.java
@@ -0,0 +1,318 @@
+package net.gotev.speechdemo;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.tbruyelle.rxpermissions.RxPermissions;
+
+import net.gotev.speech.GoogleVoiceTypingDisabledException;
+import net.gotev.speech.Speech;
+import net.gotev.speech.SpeechDelegate;
+import net.gotev.speech.SpeechRecognitionNotAvailable;
+import net.gotev.speech.SpeechUtil;
+import net.gotev.speech.TextToSpeechCallback;
+import net.gotev.speech.ui.SpeechProgressView;
+import net.gotev.toyproject.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class MainActivity extends AppCompatActivity implements SpeechDelegate {
+
+    private ImageButton button;
+    private Button speak;
+    private TextView text;
+    private TextView textResult;
+    private EditText textToSpeech;
+    private SpeechProgressView progress;
+    private LinearLayout linearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+        textResult = (TextView) findViewById(R.id.textResult);
+        button = (ImageButton) findViewById(R.id.button);
+        button.setOnClickListener(view -> onButtonClick());
+
+        speak = (Button) findViewById(R.id.speak);
+        speak.setOnClickListener(view -> onSpeakClick());
+
+        text = (TextView) findViewById(R.id.text);
+        text.setText("Dr.Jarvis");
+        textToSpeech = (EditText) findViewById(R.id.textToSpeech) ;
+        progress = (SpeechProgressView) findViewById(R.id.progress);
+        textToSpeech.setVisibility(View.GONE);
+        speak.setVisibility(View.GONE);
+       // text.setVisibility(View.GONE);
+        int[] colors = {
+                ContextCompat.getColor(this, android.R.color.black),
+                ContextCompat.getColor(this, android.R.color.darker_gray),
+                ContextCompat.getColor(this, android.R.color.black),
+                ContextCompat.getColor(this, android.R.color.holo_orange_dark),
+                ContextCompat.getColor(this, android.R.color.holo_red_dark)
+        };
+        progress.setColors(colors);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Speech.getInstance().shutdown();
+    }
+
+    private void onButtonClick() {
+        if (Speech.getInstance().isListening()) {
+            Speech.getInstance().stopListening();
+        } else {
+            RxPermissions.getInstance(this)
+                    .request(Manifest.permission.RECORD_AUDIO)
+                    .subscribe(granted -> {
+                        if (granted) { // Always true pre-M
+                            onRecordAudioPermissionGranted();
+                        } else {
+                            Toast.makeText(MainActivity.this, R.string.permission_required, Toast.LENGTH_LONG);
+                        }
+                    });
+        }
+    }
+
+    private void onRecordAudioPermissionGranted() {
+        button.setVisibility(View.GONE);
+        linearLayout.setVisibility(View.VISIBLE);
+
+        try {
+            Speech.getInstance().stopTextToSpeech();
+            Speech.getInstance().startListening(progress, MainActivity.this);
+
+        } catch (SpeechRecognitionNotAvailable exc) {
+            showSpeechNotSupportedDialog();
+
+        } catch (GoogleVoiceTypingDisabledException exc) {
+            showEnableGoogleVoiceTyping();
+        }
+    }
+
+    private void onSpeakClick() {
+        if (textToSpeech.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, R.string.input_something, Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        Speech.getInstance().say(textToSpeech.getText().toString().trim(), new TextToSpeechCallback() {
+            @Override
+            public void onStart() {
+                Toast.makeText(MainActivity.this, "TTS onStart", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCompleted() {
+                Toast.makeText(MainActivity.this, "TTS onCompleted", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError() {
+                Toast.makeText(MainActivity.this, "TTS onError", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onStartOfSpeech() {
+    }
+
+    @Override
+    public void onSpeechRmsChanged(float value) {
+        //Log.d(getClass().getSimpleName(), "Speech recognition rms is now " + value +  "dB");
+    }
+
+    @Override
+    public  void onSpeechResult(String result) {
+
+        button.setVisibility(View.VISIBLE);
+        linearLayout.setVisibility(View.GONE);
+
+        textResult.setText(result);
+
+        //this gets the speech result.
+        //pass this to the async task method along with the query.
+        //I have commeneted outthe code currently
+        //uncomment this line of code to see if its works
+        String URL = "http://ec2-54-208-79-9.compute-1.amazonaws.com:8080/hello-world";
+        new CallAPI().execute(URL,result);
+
+        if (result.isEmpty()) {
+            Speech.getInstance().say(getString(R.string.repeat));
+
+        } else {
+            //Speech.getInstance().say(result);
+        }
+    }
+
+    @Override
+    public void onSpeechPartialResults(List<String> results) {
+        //text.setText("");
+        /*for (String partial : results) {
+            text.append(partial + " ");
+        }*/
+    }
+
+    private void showSpeechNotSupportedDialog() {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        SpeechUtil.redirectUserToGoogleAppOnPlayStore(MainActivity.this);
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.speech_not_available)
+                .setCancelable(false)
+                .setPositiveButton(R.string.yes, dialogClickListener)
+                .setNegativeButton(R.string.no, dialogClickListener)
+                .show();
+    }
+
+    private void showEnableGoogleVoiceTyping() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.enable_google_voice_typing)
+                .setCancelable(false)
+                .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // do nothing
+                    }
+                })
+                .show();
+    }
+
+    public class CallAPI extends AsyncTask<String, String, String> {
+
+        public CallAPI(){
+            //set context variables if required
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+
+                URL url = new URL(params[0]); // here is your URL path
+
+                JSONObject postDataParams = new JSONObject();
+                postDataParams.put("id", 1);
+                postDataParams.put("content", params[1]);
+                Log.e("params",postDataParams.toString());
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                conn.setRequestProperty("Content-Type", "application/json");
+
+
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(postDataParams.toString());
+
+                writer.flush();
+                writer.close();
+                os.close();
+
+                int responseCode=conn.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+
+                    BufferedReader in=new BufferedReader(
+                            new InputStreamReader(
+                                    conn.getInputStream()));
+                    StringBuffer sb = new StringBuffer("");
+                    String line="";
+
+                    while((line = in.readLine()) != null) {
+
+                        sb.append(line);
+                        break;
+                    }
+
+                    in.close();
+                    return sb.toString();
+
+                }
+                else {
+                    return new String("false : "+responseCode);
+                }
+            }
+            catch(Exception e){
+                return new String("Exception: " + e.getMessage());
+            }
+
+
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            //text.setText(result);
+            try {
+                JSONObject resultobj = new JSONObject(result);
+                textResult.setText(resultobj.get("content").toString());
+                //onSpeakClick();//textToSpeech.setText();
+                Speech.getInstance().say(resultobj.get("content").toString());
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+}}
